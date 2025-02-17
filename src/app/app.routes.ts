740c4eb2a8a56dcc01237ac2dc4395cd273aae19
rishabh1S <rishabh15112001@gmail.com>
2025-02-17 19:46:14 +0530

    fix: restructure ppt-screen component and update types defination

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { PptScreenComponent } from './ppt-screen/ppt-screen.component';
+import { PptScreenComponent } from './components/ppt-screen/ppt-screen.component';
 
 export const routes: Routes = [
-
-
-    { path: '', component: LandingPageComponent }, 
-    { path: 'ppt-screen/:id', component: PptScreenComponent }
-
+  { path: '', component: LandingPageComponent },
+  { path: 'ppt-screen/:id', component: PptScreenComponent },
 ];
