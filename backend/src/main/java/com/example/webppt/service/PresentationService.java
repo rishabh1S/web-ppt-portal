e704dc94190ef8c7ed6d03be4633f01d36810a46
rishabh1S <rishabh15112001@gmail.com>
2025-02-20 15:50:17 +0530

    feat: enhance text element rendering with dynamic font size and color extraction

--- backend/src/main/java/com/example/webppt/service/PresentationService.java
+++ backend/src/main/java/com/example/webppt/service/PresentationService.java
@@ -8,7 +8,6 @@ import com.example.webppt.repository.PresentationRepository;
 
 import org.apache.poi.sl.usermodel.PaintStyle;
 import org.apache.poi.sl.usermodel.PaintStyle.SolidPaint;
-import org.apache.poi.ss.usermodel.Color;
 import org.apache.poi.xslf.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -72,64 +71,43 @@ public class PresentationService {
         return dbSlide;
     }
 
-    // private SlideElement processTextShape(XSLFTextShape textShape, Presentation presentation) {
-    //     SlideElement element = new SlideElement();
-    //     element.setType(ElementType.TEXT);
-    //     element.setContent(textShape.getText());
-
-    //     // Convert points to percentages
-    //     element.setX(convertPointsToPercentage(textShape.getAnchor().getX(), presentation.getWidth()));
-    //     element.setY(convertPointsToPercentage(textShape.getAnchor().getY(), presentation.getHeight()));
-    //     element.setWidth(convertPointsToPercentage(textShape.getAnchor().getWidth(), presentation.getWidth()));
-    //     element.setHeight(convertPointsToPercentage(textShape.getAnchor().getHeight(), presentation.getHeight()));
-
-    //     // Populate style
-    //     Map<String, Object> style = new HashMap<>();
-    //     style.put("fontSize", textShape.getTextParagraphs().get(0).getTextRuns().get(0).getFontSize());
-    //     style.put("color", textShape.getTextParagraphs().get(0).getTextRuns().get(0).getFontColor().toString());
-    //     element.setStyle(style);
-
-    //     return element;
-    // }
-
     private SlideElement processTextShape(XSLFTextShape textShape, Presentation presentation) {
         SlideElement element = new SlideElement();
         element.setType(ElementType.TEXT);
         element.setContent(textShape.getText());
-    
+
         // Convert points to percentages
         element.setX(convertPointsToPercentage(textShape.getAnchor().getX(), presentation.getWidth()));
         element.setY(convertPointsToPercentage(textShape.getAnchor().getY(), presentation.getHeight()));
         element.setWidth(convertPointsToPercentage(textShape.getAnchor().getWidth(), presentation.getWidth()));
         element.setHeight(convertPointsToPercentage(textShape.getAnchor().getHeight(), presentation.getHeight()));
-    
+
         // Populate style
         Map<String, Object> style = new HashMap<>();
         XSLFTextRun textRun = textShape.getTextParagraphs().get(0).getTextRuns().get(0);
-    
+
         style.put("fontSize", textRun.getFontSize());
-    
+
         // Extract the exact font color
         String fontColor = extractFontColor(textRun);
         style.put("color", fontColor);
-    
+
         element.setStyle(style);
         return element;
     }
 
     private String extractFontColor(XSLFTextRun textRun) {
         PaintStyle paintStyle = textRun.getFontColor();
-    
+
         if (paintStyle instanceof SolidPaint solidPaint) {
-            java.awt.Color awtColor = solidPaint.getSolidColor().getColor(); // Correct java.awt.Color usage
+            java.awt.Color awtColor = solidPaint.getSolidColor().getColor();
             if (awtColor != null) {
                 return String.format("#%02x%02x%02x", awtColor.getRed(), awtColor.getGreen(), awtColor.getBlue());
             }
         }
-    
-        return "#000000"; // Default to black if extraction fails
+
+        return "#000000";
     }
-    
 
     private SlideElement processImage(XSLFPictureShape pictureShape, Presentation presentation) throws IOException {
         SlideElement element = new SlideElement();
