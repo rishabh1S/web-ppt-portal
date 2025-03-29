1a462bebbb583b34ec2a104387719bf0fcde2ad4
rishabh1S <rishabh15112001@gmail.com>
2025-03-30 04:09:30 +0530

    feat: implement TemplateService and add significant and escalation slide generation endpoints

--- aspose-backend/src/main/java/com/example/aspose/controller/TemplateController.java
+++ aspose-backend/src/main/java/com/example/aspose/controller/TemplateController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aspose.slides.Presentation;
 import com.aspose.slides.SaveFormat;
-import com.example.aspose.service.StandardTemplateService;
+import com.example.aspose.service.TemplateService;
 import com.example.aspose.util.AsposeLicense;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,10 +18,10 @@ import jakarta.servlet.http.HttpServletResponse;
 @RequestMapping("/api/standard-slide")
 public class TemplateController {
     @Autowired
-    private StandardTemplateService standardTemplateService;
+    private TemplateService templateService;
 
-    @GetMapping
-    public void generateOverviewSlide(HttpServletResponse response) throws IOException {
+    @GetMapping("/significant")
+    public void generateSignificantSlide(HttpServletResponse response) throws IOException {
         AsposeLicense.setLicense();
         response.setContentType("application/vnd.openxmlformats-officedocument.presentationml.presentation");
         response.setHeader("Content-Disposition", "attachment; filename=presentation.pptx");
@@ -29,7 +29,27 @@ public class TemplateController {
         Presentation pres = null;
         try {
             pres = new Presentation();
-            standardTemplateService.createStandardSlide(pres);
+            templateService.createStandardSlide(pres,
+                    "SignificantMetadata.json");
+            pres.save(response.getOutputStream(), SaveFormat.Pptx);
+        } finally {
+            if (pres != null) {
+                pres.dispose();
+            }
+        }
+    }
+
+    @GetMapping("/escalation")
+    public void generateEscalationSlide(HttpServletResponse response) throws IOException {
+        AsposeLicense.setLicense();
+        response.setContentType("application/vnd.openxmlformats-officedocument.presentationml.presentation");
+        response.setHeader("Content-Disposition", "attachment; filename=presentation.pptx");
+
+        Presentation pres = null;
+        try {
+            pres = new Presentation();
+            templateService.createStandardSlide(pres,
+                    "EscalationMetadata.json");
             pres.save(response.getOutputStream(), SaveFormat.Pptx);
         } finally {
             if (pres != null) {
